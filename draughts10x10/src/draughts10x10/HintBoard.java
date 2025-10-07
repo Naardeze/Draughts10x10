554842512c29c608cec6dab6eb03e495dd43bd24
Naardeze <141658644+Naardeze@users.noreply.github.com>
2025-10-08 01:05:04 +0200

    Add files via upload

---
+++ draughts10x10/src/draughts10x10/HintBoard.java
@@ -0,0 +1,56 @@
+package draughts10x10;
+
+import static draughts10x10.Draughts10x10.HINT;
+import static draughts10x10.Draughts10x10.SQUAREBOARD;
+import static draughts10x10.SquareBoard.paintSquare;
+import java.awt.Color;
+import java.awt.Component;
+import java.awt.Graphics;
+import java.util.Set;
+
+/**
+ * HintBoard
+ * 
+ * selected (mousePressed)
+ * keySet (moveable)
+ * 
+ * @author Naardeze
+*/
+
+final class HintBoard extends Component {
+    //not selected (show moveable)
+    final static int NOT_SELECTED = -1;
+
+    private int selected;
+    private Set<Integer> keySet;
+    
+    HintBoard() {
+        setVisible(false);
+        setForeground(Color.orange);
+    }
+    
+    int getSelected() {
+        return selected;
+    }
+    
+    void setSelected(int selected) {
+        this.selected = selected;
+    }
+    
+    void setBoard(Set<Integer> keySet) {
+        selected = NOT_SELECTED;
+        this.keySet = keySet;
+        
+        setVisible(true);
+    }
+    
+    @Override
+    public void paint(Graphics g) {
+        if (selected != NOT_SELECTED) {
+            paintSquare(g, SQUAREBOARD.square[selected]);
+        } else if (HINT.isSelected()) {
+            keySet.forEach(index -> paintSquare(g, SQUAREBOARD.square[index]));
+        }
+    }
+    
+}
