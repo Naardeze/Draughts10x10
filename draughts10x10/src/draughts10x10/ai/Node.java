ef625bd6a28502a0681fb1def36618d5a9805cf5
Naardeze <141658644+Naardeze@users.noreply.github.com>
2025-10-10 17:08:54 +0200

    Add files via upload

---
+++ draughts10x10/src/draughts10x10/ai/Node.java
@@ -0,0 +1,29 @@
+package draughts10x10.ai;
+
+enum Node {
+    MIN {//beta (player)
+        @Override
+        int toAlfaBeta(int alfaBeta, int value) {
+            return Math.min(alfaBeta, value);
+        }
+
+        @Override
+        int valueOf(int value) {
+            return -value;
+        }
+    },
+    MAX {//alfa (ai)
+        @Override
+        int toAlfaBeta(int alfaBeta, int value) {
+            return Math.max(alfaBeta, value);
+        }
+
+        @Override
+        int valueOf(int value) {
+            return value;
+        }
+    };
+
+    abstract int toAlfaBeta(int alfaBeta, int value);
+    abstract int valueOf(int value);
+}
