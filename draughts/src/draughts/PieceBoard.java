b594c349c1be5322ed0b67c72acbb5a4d1a5f896
Naardeze <141658644+Naardeze@users.noreply.github.com>
2025-10-28 00:35:00 +0100

    Add files via upload

---
+++ draughts/src/draughts/PieceBoard.java
@@ -0,0 +1,85 @@
+package draughts;
+
+import static draughts.Draughts10x10.TILEBOARD;
+import static draughts.TileBoard.GRID;
+import static draughts.TileBoard.paintTile;
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Image;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
+import javax.swing.JComponent;
+
+/**
+ * PieceBoard
+ * 
+ * Board with pieces (begin position by default) and highlighted move tiles.
+ *
+ * board (begin)
+ * move (yellow, green)
+ * 
+ * @author Naardeze
+ */
+
+final class PieceBoard extends JComponent {
+    final private static char W = 'w';//hite
+    final private static char B = 'b';//lack
+
+    final static char EMPTY = '_';
+
+    final static String WB = W + "" + B;//"wb"
+
+    final static Image[][] PIECE = new Image[2][WB.length()];//[wb][WB]
+
+    final private static Color[] MOVE = {Color.yellow, Color.green};//{capture, piece}
+
+    private char[] board = new char[TILEBOARD.tile.length];//50
+    private LinkedList<Integer> move = new LinkedList();
+    
+    PieceBoard() {//begin position
+        Arrays.fill(board, 0, board.length / 2 - GRID / 2, B);//0-19
+        Arrays.fill(board, board.length / 2 - GRID / 2, board.length / 2 + GRID / 2, EMPTY);//20-29
+        Arrays.fill(board, board.length / 2 + GRID / 2, board.length, W);//30-49
+    }
+
+    char getIndex(int index) {
+        return board[index];
+    }
+    
+    void setIndex(int index, char piece) {
+        board[index] = piece;
+    }
+    
+    String getBoard() {
+        return String.valueOf(board);
+    }
+    
+    void setBoard(String board) {
+        this.board = board.toCharArray();
+    }
+    
+    LinkedList<Integer> getMove() {
+        return move;
+    }
+            
+    void setMove(LinkedList<Integer> move) {
+        this.move = move;
+    }
+    
+    @Override
+    public void paintComponent(Graphics g) {
+        for (int index : move) {
+            g.setColor(MOVE[(move.indexOf(index) + 1) / move.size()]);//[0..0, 1]
+            
+            paintTile(g, TILEBOARD.tile[index]);
+        }
+
+        for (int i = 0; i < board.length; i++) {
+            if (board[i] != EMPTY) {
+                g.drawImage(PIECE[(Character.toLowerCase(board[i]) + "" + Character.toUpperCase(board[i])).indexOf(board[i])][WB.indexOf(Character.toLowerCase(board[i]))], TILEBOARD.tile[i].x, TILEBOARD.tile[i].y, this);
+            }
+        }
+    }
+    
+}
